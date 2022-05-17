2e45e3f32ac0f7422fc43cbe9db26a75d1b5e280
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-05-17 13:03:45 -0500

    Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once.

---
+++ removing-duplicates-sorted-array.js
@@ -0,0 +1,25 @@
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+// Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+// ** Return k after placing the final result in the first k slots of nums. **
+// Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+// Constraints:
+// 1 <= nums.length <= 3 * 104
+// -100 <= nums[i] <= 100
+// nums is sorted in non-decreasing order.
+
+const removeDuplicates = (nums) => {
+  let k = 0;
+  for (let k = 0; k < nums.length; k++) {
+    let i = k + 1;
+    while (nums[k] === nums[i]) {
+      // splice to remove val at index i
+      nums.splice(i, 1);
+    }
+  }
+  console.log(nums);
+  return nums.length;
+};
+
+removeDuplicates([1, 1, 2]); // Output: 2, nums = [1,2,_]
+removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]); // Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
